d882030fceea353b301f860ba54454db7984fb1f
bm-ds-coursera <bm_ds_coursera@murphynet.id.au>
2014-07-21 13:28:53 +1000

    Added function implementation and comments for makeCacheMatrix() and cacheSolve()

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to enable caching of a solved matrix
+#
+#  Example Usage:
+#    my_matrix <- matrix(c(1, 3, 2, 4), 2, 2))
+#    my_cached_matrix <- makeCacheMatrix(my_matrix)
+#    cacheSolve(my_cached_matrix)  # calculation occurs here
+#    foo <- do_other_things()
+#    cacheSolve(my_cached_matrix)  # will return cached data FTW!
 
-## Write a short comment describing this function
+
+## makeCacheMatrix
+#
+#  Make a "cached" matrix for used with cachedSolve
+#
+#  Arguments
+#    x  initial value of matrix to use (defaults to emopty matrix)
+#
+#  Returns
+#    List of functions that will be used by cachedSolve
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inv) inverse <<- inv
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+#
+#  Solve a matrix or return a cached inverse if already solved
+#  Uses underlying solve(...) function on the "cached" matrix
+#
+#  Arguments
+#    "cached" matrix - must be a value returned from makeCacheMatrix
+#
+#  Returns
+#    inverse of matrix which may be a cached value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached data")
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
+    
 }
